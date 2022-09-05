4b733e7e8ff3c60c5bd7cf39aff64c25a6a42b44
amrusb <barto199991@gmail.com>
2022-09-05 20:54:21 +0200

    modified:   index.html 	modified:   portfolio/graphics/akatyst.jpg 	modified:   portfolio/graphics/czuwanie.jpg 	new file:   portfolio/graphics/katechezy.jpg 	deleted:    "portfolio/graphics/misje \305\233wi\304\231te.jpg" 	new file:   "portfolio/graphics/misje_\305\233wi\304\231te.jpg" 	modified:   portfolio/graphics/pielgrzymka.jpg 	modified:   portfolio/graphics/rekolekcjie_liturgiczne.jpg 	new file:   portfolio/graphics/rekolekcjie_liturgiczne_2023.jpg 	modified:   script.js 	modified:   style.css

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const galleryImages = document.querySelectorAll('.gallery img');
+
 const checkAge = () =>{  //setting my current age
     const date = new Date();
     const year = date.getFullYear();
@@ -6,16 +8,36 @@ const checkAge = () =>{  //setting my current age
     let age;
     if(month == 6 && day >=6){
         age = year - 1999;
-            console.log(age);
     }
     else if(month > 6){
             age = year - 1999;
-            console.log(age);
     }
     $(".age").text(age);
-    // ageSpan.textContent = age;
 }
 
 $(document).ready(function(){
     checkAge();
+    $('.box').click(function(){
+        this.classList.toggle('box-clicked')
+         galleryImages.forEach(img => {
+            if(img.alt !== this.children[0].alt){
+               $(img.parentElement).removeClass('box-clicked');
+            }
+        });
+    })
+    $('*').click(function(e){
+        if(!e.target.parentElement.classList.contains('box')){
+        galleryImages.forEach(img =>{
+            $(img.parentElement).removeClass('box-clicked');
+        })
+    }
+    })
+    const sign = document.querySelector('.python');
+
+    $(sign.parentElement).mouseenter(function(){
+        $(sign).attr('icon', 'logos:python')
+    })
+    $(sign.parentElement).mouseleave(function(){
+        $(sign).attr('icon', 'simple-icons:python')
+    })
 });
