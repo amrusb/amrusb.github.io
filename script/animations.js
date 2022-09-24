159cd04bc439ed5a9259fdae3e036558e6228862
amrusb <barto199991@gmail.com>
2022-09-24 19:42:58 +0200

    Animations Update

--- script/animations.js
+++ script/animations.js
@@ -1,118 +1,107 @@
-// gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
+$(".navbar-brand").click(() => {
+    gsap.to(window, {
+        duration: 1.5,
+        ease: "expo",
+        scrollTo: "body"
+    })
+})
+$(".nav li").click((section) => {
+    gsap.to(window, {
+        duration: 1,
+        ease: "power4.inOut",
+        scrollTo: {
+            y: "#" + section.target.innerHTML,
+            offsetY: 50
+        }
+    })
+})
 
-$(document).ready(function () {
-    $('.header .nav a').click((e) => {
-        switch (e.target.innerText) {
-            case "about":
-                break;
-            case "education":
-                break;
-            case "portfolio":
-                break;
-            case "contact":
-                break;
+const homeTimeline = gsap.timeline({ delay: 0.1, deafults: { duration: 3 } });
+homeTimeline.fromTo(".welcome-div",
+    {
+        yPercent: -430,
+    },
+    {
+        yPercent: 0,
+        ease: "power1.out",
+        scrollTrigger: {
+            trigger: ".welcome-div",
+            start: "center 70%",
+            end: "center top",
+            toggleActions: "play complete none none"
         }
     });
 
-    //H1
-    const mainHeading = document.querySelectorAll('h1');
-    mainHeading.forEach(header => {
-        gsap.fromTo(header,
-            {
-                x: 200,
-                opacity: 0,
-            },
-            {
-                x: 0,
-                opacity: 1,
-                duration: 0.5,
-                ease: "power4.out",
-                scrollTrigger: {
-                    id: "h1",
-                    trigger: header,
-                    start: "center 90%",
-                    end: "center 10%",
-                    toggleActions: "play complete none reset",
-                },
-                onStart: () => ScrollTrigger.refresh(),
-                onComplete: () => ScrollTrigger.refresh()
-            })
-    })
-    //H4
-    const subsectionHeading = document.querySelectorAll('h4');
-    subsectionHeading.forEach(header => {
-        gsap.fromTo(header,
-            {
-                y: 100,
-                opacity: 0
-            },
-            {
-                y: 0,
-                opacity: 1,
-                duration: 0.4,
-                scrollTrigger: {
-                    id: "h4",
-                    trigger: header,
-                    start: "center 90%",
-                    end: "center 10%",
-                    toggleActions: "play complete none reset",
-                },
-                onStart: () => ScrollTrigger.refresh(),
-                onComplete: () => ScrollTrigger.refresh()
-            })
-    })
+homeTimeline.fromTo(".photo-div",
+    {
+        yPercent: 230,
+        opacity: 0.3,
+    },
+    {
+        yPercent: 0,
+        opacity: 1,
+    }, "<");
 
-    //TIMELINE
-    const time = gsap.timeline();
-    const timelineEvent = document.querySelectorAll('.timeline-event');
-    const timelineLine = document.querySelector('.timeline');
+homeTimeline.to(".first-angles",
+    {
+        xPercent: 500,
+        ease: "power1.inOut",
+        scrollTrigger: {
+            trigger: ".home",
+            start: "70% 80%",
+            end: "bottom top",
+            scrub: 1,
+        },
 
-    gsap.from(timelineLine,
-        {
-            height: 0,
-            duration: 1,
-            ease: "bounce.inOut",
-            scrollTrigger: {
-                id: "line",
-                trigger: timelineLine,
-                start: "top 70%",
-                end: "bottom 30%",
-                toggleActions: "play complete none none",
-            },
-            onStart: () => ScrollTrigger.refresh(),
-            onComplete: () => ScrollTrigger.refresh()
+    },)
+homeTimeline.to(".second-angles",
+    {
+        yPercent: -400,
+        ease: "power1.inOut",
+        scrollTrigger: {
+            trigger: ".home",
+            start: "70% 90%",
+            end: "bottom top",
+            scrub: 1,
         }
-    );
-    timelineEvent.forEach(event => {
-        gsap.from(event,
-            {
-                opacity: 0,
-                scrollTrigger: {
-                    id: "event",
-                    trigger: event,
-                    start: "top 60%",
-                    end: "bottom 40%",
-                    toggleActions: "play complete none none",
-                },
-                onStart: () => ScrollTrigger.refresh(),
-                onComplete: () => ScrollTrigger.refresh()
-            })
+    })
+const quoteTimeline = gsap.timeline();
+quoteTimeline.from(".quote-body",
+    {
+        xPercent: -100,
+        opacity: 0,
+        scrollTrigger: {
+            trigger: ".quote-text",
+            start: "center 90%",
+            end: "center 10%",
+            toggleActions: "play reverse play reverse"
+        },
     });
+quoteTimeline.from(".quote-author", {
+    xPercent: 50,
+    opacity: 0,
+    scrollTrigger: {
+        trigger: ".quote-text",
+        start: "center 90%",
+        end: "center 10%",
+        toggleActions: "play reverse play reverse"
+    },
+}, "<");
 
-    //SKILLS
-    const skillsSection = document.querySelector('.skills');
-    const skillsItem = document.querySelectorAll(".skills .col-sm-4")
-    gsap.from(skillsItem,
-        {
-            opacity: 0,
-            toggleActions: "play pause resume reset",
-            scrollTrigger: {
-                trigger: skillsSection,
-                start: "center 70%",
-                end: "70% 30%",
-            },
-            onStart: () => ScrollTrigger.refresh(),
-            onComplete: () => ScrollTrigger.refresh()
-        }
-    );
-});
+const h1List = document.querySelectorAll("h1");
+h1List.forEach(h1 => {
+    gsap.from($(h1), {
+        xPercent: 20,
+        opacity: 0,
+        ease: "power2.out",
+        duration: 1.5,
+        scrollTrigger: {
+            trigger: h1,
+            toggleActions: "play reverse play reverse",
+            fastScrollEnd: true,
+        },
+        onStart: () => ScrollTrigger.refresh(),
+        onComplete: () => ScrollTrigger.refresh(),
+    })
+})
